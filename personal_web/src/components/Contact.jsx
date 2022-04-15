73438d7b542900b384c6f7e050621785d8b65f6f
Hector <h.marfen@gmail.com>
2022-04-15 16:50:39 +0200

    15.4.22

--- personal_web/src/components/Contact.jsx
+++ personal_web/src/components/Contact.jsx
@@ -1,6 +1,19 @@
 export default function Contact(){
     return(
-        <>
-        </>
+        <div>
+            <h3>Contact me</h3>
+            <form>
+                <label>Name</label>
+            <input
+                type='text' >    
+            </input>
+            <label>Email</label>
+            <input type='text'></input> 
+            <label>Message</label>
+            <input type='text'></input>
+            <button type='submit'>Send</button>
+            </form>
+            
+        </div>
     )
 }
