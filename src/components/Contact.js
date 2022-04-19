7c29b1d5e5712816bc9ba871ae50af8dfda3b9b5
Hector <h.marfen@gmail.com>
2022-04-19 23:02:42 +0200

    19.4.22

---
+++ src/components/Contact.js
@@ -0,0 +1,68 @@
+import '../Styles/Contact.css'
+import { useState } from 'react'
+import {send} from 'emailjs-com'
+
+
+export default function Contact(){
+
+    const [toSend, setToSend] = useState({
+        from_name,
+        e_mail,
+        message,
+    })
+
+    const onSubmit = (e) =>{
+        e.preventDefault()
+        send(
+            'service_t19nb54',
+            'template_ik0uwk8',
+            toSend,
+            'User ID'
+          )
+            .then((response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+              console.log('FAILED...', err);
+            });
+        };
+    }
+
+    const handleChange = (e) => {
+        setToSend({...toSend, [e.target.name]: e.target.value})
+    }
+
+
+    return(
+        <div className='container'>
+            <div className='row'>
+                <h2>Contact me</h2>
+            </div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    name='from_name'
+                    placeholder='Name'
+                    value={toSend.from_name}
+                    onChange={handleChange}
+                />
+                <input
+                    type='text'
+                    name='e_mail'
+                    placeholder='email'
+                    value={toSend.e_mail}
+                    onChange={handleChange}
+                />
+                <input
+                    type='text'
+                    name='message'
+                    placeholder='Your message'
+                    value={toSend.message}
+                    onChange={handleChange}
+                />
+                <button type='submit'>Submit</button>
+            </form>
+            
+        </div>
+    )
+}
